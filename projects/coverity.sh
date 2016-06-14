220c9f6483361743a50770b80a6b41c9aed8c519
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-06-15 01:17:06 +0200

    coverity WIP

--- projects/coverity.sh
+++ projects/coverity.sh
@@ -3,10 +3,12 @@ TOOL_ARCHIVE=/tmp/cov-analysis-${PLATFORM}.tgz
 TOOL_URL=https://scan.coverity.com/download/${PLATFORM}
 TOOL_BASE=/tmp/coverity-scan-analysis
 
+echo ${PLATFORM}
+
 if [ ! -d $TOOL_BASE ]; then
   # Download Coverity Scan Analysis Tool
   if [ ! -e $TOOL_ARCHIVE ]; then
-    curl -nv -O $TOOL_ARCHIVE $TOOL_URL --data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
+    curl -o $TOOL_ARCHIVE $TOOL_URL --data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
     echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
   fi
 fi
